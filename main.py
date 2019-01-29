b4ac8a6d4fe4299293e2bd5d56722f9df202d6c8
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-01-29 20:13:48 +0300

    method that creates a photo puzzle from a photo

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import os
 
 class PhotoPuzzle:
 
-    def __init__(self, source_img_path, tiles_path, pix_to_tile=4):
+    def __init__(self, source_img_path, tiles_path):
         self.image = Image.open(source_img_path)
-        self.source_img_path = source_img_path
         self.tiles_path = tiles_path
-        self.pix_to_tile = pix_to_tile
+        self.pix_to_tile = 4
+        self.tile_size = 32
 
     def load_tiles(self):
         return (os.path.join(self.tiles_path, f) for f in os.listdir(self.tiles_path) if 'jpg' in f)
@@ -17,7 +17,6 @@ class PhotoPuzzle:
         self.image.show()
 
     def get_img_mid_color(self, img, x_start=0, y_start=0, x_crop=None, y_crop=None):
-        img = Image.open(img)
         if x_crop is None:
             x_crop = img.width
         if y_crop is None:
@@ -32,21 +31,49 @@ class PhotoPuzzle:
                 green += pix[x, y][1]
                 blue += pix[x, y][2]
 
-        img.close()
         total_pix = x_crop * y_crop
         rgb = (red // total_pix, green // total_pix, blue // total_pix)
 
         return rgb
+
+    def distance_between_colors(self, color_1, color_2):
+        return sum(map(lambda x, y: (y-x)**2, color_1, color_2))
+
+    def create_photo_puzzle(self, pix_to_tile=4):
+        self.pix_to_tile = pix_to_tile
+        photo_puzzle = Image.new(mode="RGB", size=(self.image.width // self.pix_to_tile * self.tile_size,
+                                                   self.image.height // self.pix_to_tile * self.tile_size))
+        tiles_mid_colors = []
+        for t in list(puzzle.load_tiles()):
+            tiles_mid_colors.append((t, puzzle.get_img_mid_color(Image.open(t), 0, 0, self.tile_size, self.tile_size)))
+
+        tile_filename = None
+        for x in range(0, self.image.width, self.pix_to_tile):
+            for y in range(0, self.image.height, self.pix_to_tile):
+                min_distance = 3 * 256**2
+                crop_mid_color = self.get_img_mid_color(self.image, x, y, self.pix_to_tile, self.pix_to_tile)
+                
+                for color in tiles_mid_colors:
+                    distance = self.distance_between_colors(color[1], crop_mid_color)
+                    if distance < min_distance:
+                        min_distance = distance
+                        tile_filename = color[0]
+
+                tile_img = Image.open(tile_filename)
+                photo_puzzle.paste(im=tile_img, box=(x // self.pix_to_tile * self.tile_size,
+                                                     y // self.pix_to_tile * self.tile_size,
+                                                     x // self.pix_to_tile * self.tile_size + self.tile_size,
+                                                     y // self.pix_to_tile * self.tile_size + self.tile_size))
+            print(str(round(x / self.image.width * 100)) + "% completed")
+        base, puzzle_name = os.path.split(self.image.filename)
+        path_to_save = os.path.join(base, 'puzzle-{0}-{1}'.format(self.pix_to_tile, puzzle_name))
+        photo_puzzle.save(path_to_save)
+        return path_to_save
 
 
 if __name__ == "__main__":
     photo_path = os.path.abspath('frog.png')
     folder = os.path.abspath('tiles/')
 
-    puzzle = PhotoPuzzle(photo_path, folder, 8)
-    puzzle.show_source_img()
-    print(puzzle.get_img_mid_color(photo_path))
-
-    tile_size = (0, 0, 32, 32)
-    for t in list(puzzle.load_tiles()):
-        print(t, puzzle.get_img_mid_color(t, *tile_size))
+    puzzle = PhotoPuzzle(photo_path, folder)
+    puzzle.create_photo_puzzle(pix_to_tile=8)
