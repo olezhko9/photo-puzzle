efb19a6e16c254db4d295053f448ca2de1ce1a8a
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-07-18 00:19:14 +0300

    refactoring with eslint rules

--- client/src/components/Mosaic.vue
+++ client/src/components/Mosaic.vue
@@ -1,82 +1,82 @@
 <template>
   <div class="container">
-  	<div class="columns">
-	  	<div class="column">
-		    <b-field>
-				<b-upload v-model="file" class="is-info" drag-drop @input="onFileChange">
-				    <section class="section">
-				        <div class="has-text-centered">
-				            <p>
-				                <b-icon
-				                    icon="upload"
-				                    size="is-large">
-				                </b-icon>
-				            </p>
-				            <p>Перетащите сюда файл,</p> 
-				            <p>который хотите обработать</p>
-				        </div>
-				    </section>
-				</b-upload>
-			</b-field>
-			
-		    <div v-if="file" class="control">
-		        <b-tag 
-		        	type="is-primary" 
-		        	size="is-medium"
-		        	@close="clearAll" 
-		        	attached closable>
-		        	{{ file.name }}
-		        </b-tag>
-		        <b-button type="is-primary" @click="uploadImage">Отправить</b-button>
-		    </div>
-		</div>
+    <div class="columns">
+      <div class="column">
+        <b-field>
+        <b-upload v-model="file" class="is-info" drag-drop @input="onFileChange">
+          <section class="section">
+            <div class="has-text-centered">
+              <p>
+                <b-icon
+                  icon="upload"
+                  size="is-large">
+                </b-icon>
+               </p>
+               <p>Перетащите сюда файл,</p>
+               <p>который хотите обработать</p>
+            </div>
+          </section>
+        </b-upload>
+      </b-field>
 
-	    <div class="column" id="preview">
-	    	<img v-if="url" :src="url" />
-	    	<img v-if="mosaicUrl" :src="mosaicUrl">
-	  	</div>
-  	</div>
+        <div v-if="file" class="control">
+          <b-tag
+            type="is-primary"
+            size="is-medium"
+            @close="clearAll"
+            attached closable>
+            {{ file.name }}
+          </b-tag>
+          <b-button type="is-primary" @click="uploadImage">Отправить</b-button>
+        </div>
+    </div>
+
+      <div class="column" id="preview">
+        <img v-if="url" :src="url" />
+        <img v-if="mosaicUrl" :src="mosaicUrl">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from 'axios'
 
 export default {
   name: 'Mosaic',
-  data() {
+  data () {
     return {
       file: null,
       url: '',
       mosaicUrl: ''
-    };
+    }
   },
 
-methods: {
-	onFileChange(e) {
-      this.url = URL.createObjectURL(this.file);
+  methods: {
+    onFileChange (e) {
+      this.url = URL.createObjectURL(this.file)
       this.mosaicUrl = ''
     },
 
-  	uploadImage() {
-  		let formData = new FormData()
-  		formData.append("file", this.file)
-		axios.post('http://127.0.0.1:5000/upload', formData, {
-			headers: {
-	          'Content-Type': 'multipart/form-data'
-	        }
-		}).then(res => {
-			this.mosaicUrl = 'http://127.0.0.1:5000/download/' + res.data.filename
-		})
-  	},
+    uploadImage () {
+      let formData = new FormData()
+      formData.append('file', this.file)
+      axios.post('http://127.0.0.1:5000/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      }).then(res => {
+        this.mosaicUrl = 'http://127.0.0.1:5000/download/' + res.data.filename
+      })
+    },
 
-  	clearAll() {
-  		this.file = null
-  		this.url = '',
-  		this.mosaicUrl = ''
-  	}
+    clearAll () {
+      this.file = null
+      this.url = ''
+      this.mosaicUrl = ''
+    }
+  }
 }
-};
 </script>
 
 <style scoped>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -14,7 +14,7 @@ export default new Router({
       path: '/mosaic',
       name: 'Mosaic',
       component: Mosaic
-    },
+    }
   ],
   mode: 'history'
 })
